2117ba9fed2f6f3a6f6c9b7dcba4ced7fa2a1072
eshrak <eshrak@Mds-MacBook-Air-2.local>
2025-07-31 00:09:29 +0600

    desktop desing done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        fontvar: ['fontvar', 'sans-serif'],
+        fontvaritalic: ['fontvaritalic', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
